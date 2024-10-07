1063e3024d3f74a9af58f9928f123a8db9a89ffa
nicolevenachi <ludwing@unicauca.edu.co>
2024-10-07 08:44:35 -0500

    01_FlappyBirdClone_07_KeyboardEvents->Flappy funcionality

--- 01_FlappyBird/src/index.js
+++ 01_FlappyBird/src/index.js
@@ -8,7 +8,7 @@ const config = {
   physics: {
     default: 'arcade',
     arcade: {
-      // gravity: { y: 50 }
+      gravity: { y: 400 },
       debug:true,
     }
   },
@@ -35,23 +35,39 @@ function preload () {
 
 let bird = null;
 const VELOCITY = 200;
+let flapVelocity = 150;
+
 function create () {
   this.add.image(0,0, 'sky').setOrigin(0,0); 
 
   bird = this.physics.add.sprite(15, config.height/2, 'birdSquare').setOrigin(0.5,0.5) // initializing object with physics
-  bird.body.velocity.x = VELOCITY
-  console.log(bird.body.x);
+  bird.body.velocity.x = 100
+
+  // events
+  this.input.on('pointerdown', function() {
+    console.log('mouse pointer pressed');
+  });
+
+  // keyboard events
+  this.input.keyboard.on('keydown-SPACE', flap);
   
 }
 
 function update(time, delta) {
 
   // repeat feature -> if bird gets out from the screen, take it back to the start position
-  // if (bird.body.x >= config.width) bird.body.x = 15
-  // if (bird.body.x <= 0) bird.body.x = config.width
+  if (bird.body.y >= config.height) bird.body.y = bird.height/2
+  if (bird.body.y <= 0) bird.body.y = config.height - bird.height  
 
   // bounce feature (move back and forth) -> bounce in the walls
   if (bird.body.x >= config.width-bird.width) bird.body.velocity.x = -VELOCITY
   if (bird.body.x < 0) bird.body.velocity.x = VELOCITY
+
+  // reduce aceleration 
+  if (bird.body.velocity.y > 400) bird.body.velocity.y = 0
+}
+
+function flap() {
+  bird.body.velocity.y = -flapVelocity
 }
 
