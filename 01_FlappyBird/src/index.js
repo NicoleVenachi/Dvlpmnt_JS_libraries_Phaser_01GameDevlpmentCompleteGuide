39d84cbc518e3f4ae374451ff43883573f684472
nicolevenachi <ludwing@unicauca.edu.co>
2024-10-06 20:01:22 -0500

    01_FlappyBirdClone_04_FisicasVelocidad

--- 01_FlappyBird/src/index.js
+++ 01_FlappyBird/src/index.js
@@ -31,12 +31,20 @@ function preload () {
   // load image, specify asset
   // (resourceFutureId, path)
   this.load.image('sky', 'assets/sky.png');
+  this.load.image('birdSquare', 'assets/bird.png');
 }
+
+let bird = null;
 
 function create () {
   // addd image to the scene, specify resource
   //(x,y, key)
   // x -=> 400. y -> 300
   this.add.image(0,0, 'sky').setOrigin(0,0);
+
+
+  //iknicializar Sprite (pongo en mitad del screen, y su posicion que sea la mitad de la img)
+  bird = this.add.sprite(config.width * 0.1, config.height/2, 'birdSquare').setOrigin(0.5,0.5)
+  debugger
 }
 
