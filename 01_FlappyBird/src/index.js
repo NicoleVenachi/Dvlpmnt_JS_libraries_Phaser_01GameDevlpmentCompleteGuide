2af76d5992b27466f2caf085f1cf91a19c136752
nicolevenachi <ludwing@unicauca.edu.co>
2024-11-08 20:01:52 -0500

    FlappyBirdClone_18->Shared config

--- 01_FlappyBird/src/index.js
+++ 01_FlappyBird/src/index.js
@@ -2,17 +2,25 @@
 import Phaser from "phaser";
 import PlayScene from "./scenes/PlayScene";
 
+const WIDTH = 800;
+const HEIGHT = 600;
+const BIRD_POSITION =  {x:15, y:HEIGHT/2};
+
+const SHARED_CONFIG = {
+  width: WIDTH, 
+  height: HEIGHT,
+  startPosition: BIRD_POSITION,
+}
 const config = {
   type: Phaser.AUTO, //render tyoe (WebGL)
-  width: 800,
-  height: 600,
+  ...SHARED_CONFIG,
   physics: {
     default: 'arcade',
     arcade: {
       debug:true,
     }
   },
-  scene: [PlayScene]
+  scene: [new PlayScene(SHARED_CONFIG)]
 };
 
 let bird = null;
--- 01_FlappyBird/src/scenes/PlayScene.js
+++ 01_FlappyBird/src/scenes/PlayScene.js
@@ -1,25 +1,22 @@
 import Phaser from 'phaser';
 
 class PlayScene extends Phaser.Scene {
-  constructor() {
+  constructor(config) {
     super('PlayScene'); //ejecuto el custructor de Phaser Scene
 
-    this.initialBirdPosition = {
-      x:15, 
-      y:400,
-    };
+    this.config = config;
     this.bird = null;
   }
 
   preload() {
     this.load.image('sky', 'assets/sky.png');
-    this.load.image('birdSquare', 'assets/bird.png');
+    this.load.image('birdSquare', 'assets/bird.png');  
   }
 
   create() {
     this.add.image(0,0, 'sky').setOrigin(0,0); 
 
-    this.bird = this.physics.add.sprite(this.initialBirdPosition.x, this.initialBirdPosition.y, 'birdSquare').setOrigin(0.5,0.5)
+    this.bird = this.physics.add.sprite(this.config.startPosition.x, this.config.startPosition.y, 'birdSquare').setOrigin(0.5,0.5)
     this.bird.body.velocity.x = 100
     this.bird.body.gravity.y = 400
   }
