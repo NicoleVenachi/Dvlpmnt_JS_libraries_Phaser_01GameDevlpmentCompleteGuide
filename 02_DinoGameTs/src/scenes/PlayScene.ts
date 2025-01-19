98d45504fb098c5d63321ad4ad3ca1bdd4a9fa95
nicolevenachi <ludwing@unicauca.edu.co>
2025-01-18 19:40:13 -0500

    02_DinoGameClone->06-CreatePlayer

--- 02_DinoGameTs/src/scenes/PlayScene.ts
+++ 02_DinoGameTs/src/scenes/PlayScene.ts
@@ -12,9 +12,20 @@ class PlayScene extends Phaser.Scene {
 
   // ************** Phaser methods **************
   create() {
+   this.createEnvironment();
+   this.createPlayer();
+
+  
+  }
+
+  // ************** Custom methods **************
+  createEnvironment() {
     this.add.tileSprite(0, this.gameHeight, 88, 26, 'ground').setOrigin(0, 1);
   }
 
+  createPlayer() {
+    this.physics.add.sprite(0, this.gameHeight, 'dino-idle').setOrigin(0, 1);
+  }
 }
 
 export default PlayScene;
